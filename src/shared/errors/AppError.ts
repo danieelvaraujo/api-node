31263bd3a6e570b71d3259d119f3ab1c7570102d
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-01 14:49:29 -0300

    refactor: fixed typo in app error

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,10 +1,10 @@
 class AppError {
   public readonly message: string;
-  public readonly statuscCode: number;
+  public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
     this.message = message;
-    this.statuscCode = statusCode;
+    this.statusCode = statusCode;
   }
 }
 
