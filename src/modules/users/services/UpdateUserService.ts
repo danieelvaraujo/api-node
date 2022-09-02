03317a88a648147e6984cd3c771903dbd857a6e1
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-01 21:26:06 -0300

    feat: update user service

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  password: string;
+}
+
+class UpdateUserService {
+  public async execute({ id, name, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User now found.');
+    }
+
+    user.name = name;
+    user.password = password;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserService;
