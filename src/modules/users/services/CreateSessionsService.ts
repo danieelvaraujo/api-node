e0979b717f116772b67a07463f654f7f497f25cf
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-02 16:57:34 -0300

    refactor: code style

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -21,13 +21,11 @@ class CreateSessionsService {
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
-
     if (!user) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordConfirmed = await compare(password, user.password);
-
     if (!passwordConfirmed) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
