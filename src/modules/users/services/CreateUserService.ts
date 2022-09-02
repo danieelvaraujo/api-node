987970ea1ade7a73e8be159f6575e83f9d5fe515
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-02 16:58:41 -0300

    refactor: code style

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -15,13 +15,11 @@ class CreateUserService {
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
     const emailExists = await usersRepository.findByEmail(email);
-
     if (emailExists) {
       throw new AppError('Email address already used.');
     }
 
     const hashedPassword = await hash(password, 8);
-
     const user = usersRepository.create({
       name,
       email,
