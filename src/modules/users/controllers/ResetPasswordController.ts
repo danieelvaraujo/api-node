c9bb48fbe9a842fb1a5a583b94ebd42205f4250e
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-02 18:05:31 -0300

    refactor: code style

--- src/modules/users/controllers/ResetPasswordController.ts
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -3,9 +3,9 @@ import ResetPasswordService from '../services/ResetPasswordService';
 
 class ResetPasswordController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { password, token } = request.body;
+    const { token, password } = request.body;
     const resetPassword = new ResetPasswordService();
-    await resetPassword.execute({ password, token });
+    await resetPassword.execute({ token, password });
 
     return response.status(204).json();
   }
