c8be62d01f4a411f246ce9bc92e0c317b4c64e6d
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-06 16:17:04 -0300

    feat: list products with cache configuration

--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -2,11 +2,20 @@ import { getCustomRepository } from 'typeorm';
 
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductsService {
   public async execute(): Promise<Product[]> {
+    const redisProductListKey = 'api-vendas-PRODUCT_LIST';
     const productsRepository = getCustomRepository(ProductRepository);
-    const products = productsRepository.find();
+
+    const redisCache = new RedisCache();
+    let products = await redisCache.recover<Product[]>(redisProductListKey);
+    if (!products) {
+      products = await productsRepository.find();
+
+      await redisCache.save(redisProductListKey, products);
+    }
 
     return products;
   }
