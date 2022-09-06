0b54bd4146945bfd15cba94b9a0d2ed984ca959e
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-06 11:30:05 -0300

    feat: added order routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -6,6 +6,7 @@ import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customerRouter from '@modules/customers/routes/customer.routes';
+import ordersRouter from '@modules/orders/routes/order.routes';
 
 const routes = Router();
 routes.use('/products', productsRouter);
@@ -14,5 +15,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customerRouter);
+routes.use('/order', ordersRouter);
 
 export default routes;
