7f4afdbc3641354f854da1031cd5953651fe7376
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-06 16:30:12 -0300

    feat: invalidating cache if a product is deleted

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -2,6 +2,8 @@ import { getCustomRepository } from 'typeorm';
 
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
 import AppError from '@shared/errors/AppError';
+import RedisCache from '@shared/cache/RedisCache';
+import { redisEnv } from '@shared/redis-env';
 
 interface IRequest {
   id: string;
@@ -10,12 +12,14 @@ interface IRequest {
 class DeleteProductService {
   public async execute({ id }: IRequest): Promise<void> {
     const productsRepository = getCustomRepository(ProductRepository);
+    const redisCache = new RedisCache();
     const product = await productsRepository.findOne(id);
 
     if (!product) {
       throw new AppError('Product now found.');
     }
 
+    await redisCache.invalidade(redisEnv.productListKey);
     await productsRepository.remove(product);
   }
 }
