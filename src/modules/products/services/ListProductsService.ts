ca86acca5a2d7b67985cb4b94f92bff6151cf9f5
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-06 16:27:50 -0300

    refactor: using redis-env for cache key in list products service

--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -3,18 +3,18 @@ import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
 import RedisCache from '@shared/cache/RedisCache';
+import { redisEnv } from '@shared/redis-env';
 
 class ListProductsService {
   public async execute(): Promise<Product[]> {
-    const redisProductListKey = 'api-vendas-PRODUCT_LIST';
     const productsRepository = getCustomRepository(ProductRepository);
 
     const redisCache = new RedisCache();
-    let products = await redisCache.recover<Product[]>(redisProductListKey);
+    let products = await redisCache.recover<Product[]>(redisEnv.productListKey);
     if (!products) {
       products = await productsRepository.find();
 
-      await redisCache.save(redisProductListKey, products);
+      await redisCache.save(redisEnv.productListKey, products);
     }
 
     return products;
