5397a2f464a5ccf438ffb0430cfc9eab972024f3
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-06 17:00:11 -0300

    chore: using rate limiter middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -8,12 +8,14 @@ import { pagination } from 'typeorm-pagination';
 
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 import uploadConfig from '@config/upload';
 import '@shared/typeorm';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
