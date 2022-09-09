9db4c8e0e5456e464477adc0927eb54cd4edb61e
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-09 09:59:33 -0300

    chore: using container to inject dependecies of bcrypt

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,7 @@ import { container } from 'tsyringe';
 
 import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
 import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
+import '@modules/users/providers';
 
 container.registerSingleton<ICustomersRepository>(
   'CustomersRepository',
