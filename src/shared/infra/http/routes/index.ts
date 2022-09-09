130defb8953524c1be3e9056ae341725c4487c7c
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-09 11:16:13 -0300

    fix: type in orders router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,7 +6,7 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import customerRouter from '@modules/customers/infra/http/routes/customer.routes';
-import ordersRouter from '@modules/orders/infra/http/routes/order.routes';
+import ordersRouter from '@modules/orders/infra/http/routes/orders.routes';
 
 const routes = Router();
 routes.use('/products', productsRouter);
@@ -15,6 +15,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customerRouter);
-routes.use('/order', ordersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
