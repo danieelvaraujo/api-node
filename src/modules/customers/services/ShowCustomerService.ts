ced0491cecfaef5b3d343cde5586c825bbca25eb
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-09 10:45:43 -0300

    chore: show customer service with injectable repository

--- src/modules/customers/services/ShowCustomerService.ts
+++ src/modules/customers/services/ShowCustomerService.ts
@@ -1,17 +1,19 @@
 import AppError from '@shared/errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { inject, injectable } from 'tsyringe';
 
+import { IFindCustomer } from '../domain/models/IFindCustomer';
+import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
 import Customer from '../infra/typeorm/entities/Customer';
 import CustomersRepository from '../infra/typeorm/repositories/CustomersRepository';
 
-interface IRequest {
-  id: string;
-}
-
+@injectable()
 class ShowCustomerService {
-  public async execute({ id }: IRequest): Promise<Customer> {
-    const customersRepository = getCustomRepository(CustomersRepository);
-    const customer = await customersRepository.findById(id);
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+  public async execute({ id }: IFindCustomer): Promise<Customer> {
+    const customer = await this.customersRepository.findById(id);
     if (!customer) {
       throw new AppError('Customer not found.');
     }
