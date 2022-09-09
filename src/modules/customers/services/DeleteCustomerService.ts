381b012998f107019ef17a44addb921fd088b304
Daniel Valente <danieelvaraujo@hotmail.com>
2022-09-09 10:28:20 -0300

    chore: delete customer interface and using injectable repository

--- src/modules/customers/services/DeleteCustomerService.ts
+++ src/modules/customers/services/DeleteCustomerService.ts
@@ -1,21 +1,24 @@
 import AppError from '@shared/errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { injectable, inject } from 'tsyringe';
 
 import CustomersRepository from '../infra/typeorm/repositories/CustomersRepository';
+import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
+import { IDeleteCustomer } from '../domain/models/IDeleteCustomer';
 
-interface IRequest {
-  id: string;
-}
-
+@injectable()
 class DeleteCustomerService {
-  public async execute({ id }: IRequest): Promise<void> {
-    const customersRepository = getCustomRepository(CustomersRepository);
-    const customer = await customersRepository.findById(id);
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ id }: IDeleteCustomer): Promise<void> {
+    const customer = await this.customersRepository.findById(id);
     if (!customer) {
       throw new AppError('Customer not found.');
     }
 
-    await customersRepository.remove(customer);
+    await this.customersRepository.remove(customer);
   }
 }
 
